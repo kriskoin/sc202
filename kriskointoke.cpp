1892a5f688962254d0cd60d6e70e0c45e75d6697
kriskoin <cris7775@gmail.com>
2023-01-06 23:40:06 -0600

    update

--- kriskointoke.cpp
+++ kriskointoke.cpp
@@ -5,15 +5,15 @@
 using namespace eosio;
 
 
-    TABLE token_settings {
-        std::string  token_symbol;
-        uint8_t withdraw_timespan;//in minutes
-    } ;
-
-    using singleton_settings = eosio::singleton<"tok.settings"_n,token_settings> ;
-    
-
 CONTRACT kriskointoke : public contract {
+
+     TABLE tokesettings {
+        std::string  token_symbol ;
+        uint32_t withdraw_timespan ;//in minutes
+    } default_settings;
+
+    using singleton_settings = eosio::singleton<"tokesettings"_n,tokesettings> ;
+
     public:
         using contract::contract;
 
@@ -31,8 +31,8 @@ CONTRACT kriskointoke : public contract {
         TABLE balances {
             name acct;
             std::string sym;
-
-            int64_t funds = 0;;
+            int64_t funds = 0;
+            uint32_t withdraw_due_time;
             auto primary_key() const { return acct.value; }
         };
 
@@ -42,11 +42,11 @@ CONTRACT kriskointoke : public contract {
                      name to, 
                      eosio::asset quantity, 
                      std::string memo){
-            
-            eosio::symbol token_symbol("MYCASH", 0);
+
+            eosio::symbol token_symbol(get_token_symbol(),0);
             check(to != get_self(),"No transfer yourself");
             check (quantity.amount > 0,"no negative values");
-            check(quantity.symbol != token_symbol, "Illegal asset symbol");
+            check(quantity.symbol.code().to_string() == token_symbol.code().to_string(), "Illegal asset symbol");
 
             balances_table balanceTbl(get_self(), from.value);
             auto it = balanceTbl.find(to.value);
@@ -81,6 +81,31 @@ CONTRACT kriskointoke : public contract {
                 std::make_tuple(from,to,quantity,std::string("send u back"))
             ).send();
         }
+
+        ACTION setconfig(std::string  symbol,uint8_t timespan){
+            auto config = settings_instance.get_or_create(get_self(),default_settings);
+            config.token_symbol = symbol;
+            config.withdraw_timespan = timespan;
+            settings_instance.set(config,get_self());
+           
+        }
+
+        uint32_t get_timespan (){
+            if( settings_instance.exists()){
+                return settings_instance.get().withdraw_timespan;
+            }else{
+                return 0;
+            }    
+        } 
+
+        std::string get_token_symbol (){
+            if( settings_instance.exists()){
+                return settings_instance.get().token_symbol;
+            }else{
+                return "NOSYM";
+            }    
+        } 
+        
 
 };
 
