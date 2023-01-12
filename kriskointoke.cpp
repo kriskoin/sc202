5a1866d81bb285c7a9e54b54390b2bb0bed61894
kriskoin <cris7775@gmail.com>
2023-01-11 23:13:18 -0600

    update

--- kriskointoke.cpp
+++ kriskointoke.cpp
@@ -3,7 +3,7 @@
 #include <eosio/singleton.hpp>
 
 using namespace eosio;
-
+const static uint32_t MINUTE = 60;
 
 CONTRACT kriskointoke : public contract {
 
@@ -48,18 +48,21 @@ CONTRACT kriskointoke : public contract {
             check (quantity.amount > 0,"no negative values");
             check(quantity.symbol.code().to_string() == token_symbol.code().to_string(), "Illegal asset symbol");
 
-            balances_table balanceTbl(get_self(), from.value);
+            balances_table balanceTbl(get_self(), get_self().value);
             auto it = balanceTbl.find(to.value);
-
+            eosio::time_point_sec tps = eosio::current_time_point();
+        
             if (it != balanceTbl.end()){
                 balanceTbl.modify(it, get_self(), [&](auto &row) {
                 row.funds += quantity.amount;
+                row.withdraw_due_time  = tps.sec_since_epoch() + (get_timespan() *  MINUTE) ;
                 });
             }else{
                 balanceTbl.emplace(get_self(), [&](auto &row) {
                 row.acct = to;
                 row.funds = quantity.amount;
                 row.sym = token_symbol.code().to_string();
+                row.withdraw_due_time  = tps.sec_since_epoch() + (get_timespan() * MINUTE );
                 });
             }
             
@@ -72,14 +75,23 @@ CONTRACT kriskointoke : public contract {
                      eosio::asset quantity, 
                      std::string memo){
 
-            
+            eosio::symbol token_symbol(get_token_symbol(),0);
+            check(quantity.symbol.code().to_string() == token_symbol.code().to_string(), "Illegal asset symbol");
 
+            balances_table balanceTbl(get_self(), get_self().value);
+            auto it = balanceTbl.find(to.value);
+            //check (it != balanceTbl.end(),"Couldn't find balance for this account");
+            
+            eosio::time_point_sec tps = eosio::current_time_point();
+            check (  it->withdraw_due_time <= tps.sec_since_epoch() ,"too soon for withdraw");
             action(
                 permission_level {get_self(),"active"_n},
                 "eosio.token"_n,
                 "transfer"_n,
                 std::make_tuple(from,to,quantity,std::string("send u back"))
             ).send();
+            
+            
         }
 
         ACTION setconfig(std::string  symbol,uint8_t timespan){
